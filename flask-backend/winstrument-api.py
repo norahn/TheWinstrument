b227f4aec13c61818132868578ed12e299374fda
Sebastian Gustafsson <carlsebastiangustafsson@gmail.com>
2020-02-22 17:26:51 +0000

    flask backend

---
+++ flask-backend/winstrument-api.py
@@ -0,0 +1,26 @@
+import json
+import flask
+from flask import request, jsonify, send_from_directory
+from soundhandler import *
+
+app = flask.Flask(__name__, static_url_path='')
+app.config["DEBUG"] = True
+
+@app.route('/sounds/<path:path>')
+def send_sound(path):
+    print(path)
+    return send_from_directory('sounds', path)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''HELLO'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/sounds/all', methods=['GET'])
+def api_all():
+    return jsonify(getAllAvailableSounds())
+
+
+app.run(host= '0.0.0.0')
