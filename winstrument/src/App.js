497116b60a0aae83e67da80f83be8459f5530cb9
norahn <naver.norah@gmail.com>
2020-02-22 15:49:02 +0000

    added button

--- winstrument/src/App.js
+++ winstrument/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button color={"yellow"}/>
+        <Button color={"red"}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ winstrument/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './button.css';
+
+class Button extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+  render() {
+    const { color } = this.props;
+    return (
+      <svg className="vinylBtn" style={{color:this.props.color, width:"20%", height:"20%"}} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="record-vinyl"
+      className="svg-inline--fa fa-record-vinyl fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512">
+      <path fill="currentColor" d="M256 152a104 104 0 1 0 104 104 104 104 0 0 0-104-104zm0 128a24
+      24 0 1 1 24-24 24 24 0 0 1-24 24zm0-272C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 376a128 128 0 1 1 128-128 128 128 0 0 1-128 128z">
+      </path></svg>
+    );
+  }
+}
+export default Button;
